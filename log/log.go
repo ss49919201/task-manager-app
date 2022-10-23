e907203079d6a4fc3bcba5c89ab4c9cbf918e5c2
s-beats <sakashin10291029@gmail.com>
2022-10-23 21:40:29 +0900

    setup logger

--- log/log.go
+++ log/log.go
@@ -1,3 +1,44 @@
 package log
 
-type Logger interface{}
+import (
+	"os"
+	"sync"
+
+	"github.com/rs/zerolog"
+)
+
+var (
+	logger   zerolog.Logger
+	syncOnce sync.Once
+)
+
+func setup() {
+	syncOnce.Do(func() {
+		logger = zerolog.New(zerolog.ConsoleWriter{Out: os.Stderr})
+	})
+}
+
+func Fatal() *zerolog.Event {
+	setup()
+	return logger.Fatal()
+}
+
+func Error() *zerolog.Event {
+	setup()
+	return logger.Error()
+}
+
+func Warn() *zerolog.Event {
+	setup()
+	return logger.Warn()
+}
+
+func Debug() *zerolog.Event {
+	setup()
+	return logger.Debug()
+}
+
+func Info() *zerolog.Event {
+	setup()
+	return logger.Info()
+}
