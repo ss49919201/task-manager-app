0c33e8381f130a704bb1da84f52fc45f4f2f36d1
s-beats <sakashin10291029@gmail.com>
2022-10-30 22:15:40 +0900

    router test

--- server/router.go
+++ server/router.go
@@ -1,23 +1,62 @@
 package server
 
-import "net/http"
+import (
+	"net/http"
+	"sync"
+)
 
 type middleware func(http.HandlerFunc) http.HandlerFunc
 
 type router struct {
+	handlersGET         map[string]http.HandlerFunc
+	handlersPOST        map[string]http.HandlerFunc
 	middlewareFunctions []middleware
+
+	rwMu sync.RWMutex
 }
 
 func NewRouter() *router {
-	return &router{}
+	return &router{
+		handlersGET:         make(map[string]http.HandlerFunc),
+		handlersPOST:        make(map[string]http.HandlerFunc),
+		middlewareFunctions: []middleware{},
+	}
+}
+
+func (r *router) handlers(method string) map[string]http.HandlerFunc {
+	switch method {
+	case http.MethodGet:
+		return r.handlersGET
+	case http.MethodPost:
+		return r.handlersPOST
+	default:
+		return nil
+	}
 }
 
 func (r *router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	// TODO:
+	r.rwMu.RLock()
+	defer r.rwMu.RUnlock()
+
+	handlers := r.handlers(req.Method)
+	fn, ok := handlers[req.URL.Path]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	for _, m := range r.middlewareFunctions {
+		fn = m(fn)
+	}
+
+	fn(w, req)
 }
 
 // TODO: 重複の対応
 func (r *router) SetMiddleware(m middleware) *router {
+	r.rwMu.Lock()
+	defer r.rwMu.Unlock()
+
 	if m == nil {
 		return r
 	}
@@ -27,8 +66,15 @@ func (r *router) SetMiddleware(m middleware) *router {
 }
 
 func (r *router) Get(pattern string, fn http.HandlerFunc) {
-	// TODO:
+	r.rwMu.Lock()
+	defer r.rwMu.Unlock()
+
+	r.handlersGET[pattern] = fn
 }
+
 func (r *router) Post(pattern string, fn http.HandlerFunc) {
-	// TODO:
+	r.rwMu.Lock()
+	defer r.rwMu.Unlock()
+
+	r.handlersPOST[pattern] = fn
 }
--- server/router_test.go
+++ server/router_test.go
@@ -1,11 +1,111 @@
 package server
 
 import (
+	"bytes"
+	"io"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func Test_router(t *testing.T) {
+	r := NewRouter()
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("X-Hello", "Hello, World!")
+		w.Write([]byte("Hello, World!"))
+		w.WriteHeader(http.StatusOK)
+	})
+
+	testServer := httptest.NewServer(r)
+	defer testServer.Close()
+
+	resp, err := http.Get(testServer.URL)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	assert.Equal(t, "Hello, World!", resp.Header.Get("X-Hello"))
+	respBody, err := io.ReadAll(resp.Body)
+	assert.NoError(t, err)
+	defer resp.Body.Close()
+	assert.Equal(t, "Hello, World!", string(respBody))
+}
+
+func Test_router_GET(t *testing.T) {
+	r := NewRouter()
+	r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("pong"))
+		w.WriteHeader(http.StatusOK)
+	})
+
+	testServer := httptest.NewServer(r)
+	defer testServer.Close()
+
+	// GET
+	resp, err := http.Get(testServer.URL + "/ping")
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	respBody, err := io.ReadAll(resp.Body)
+	assert.NoError(t, err)
+	defer resp.Body.Close()
+	assert.Equal(t, "pong", string(respBody))
+
+	resp, err = http.Get(testServer.URL + "/not_found")
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusNotFound, resp.StatusCode)
+	respBody, err = io.ReadAll(resp.Body)
+	assert.NoError(t, err)
+	defer resp.Body.Close()
+	assert.Equal(t, "", string(respBody))
+
+	// POST
+	resp, err = http.Post(testServer.URL, "application/json", nil)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusNotFound, resp.StatusCode)
+	respBody, err = io.ReadAll(resp.Body)
+	assert.NoError(t, err)
+	defer resp.Body.Close()
+	assert.Equal(t, "", string(respBody))
+}
+
+func Test_router_POST(t *testing.T) {
+	r := NewRouter()
+	r.Post("/test", func(w http.ResponseWriter, r *http.Request) {
+		b, err := io.ReadAll(r.Body)
+		assert.NoError(t, err)
+		w.Write([]byte("success: " + string(b)))
+		w.WriteHeader(http.StatusOK)
+	})
+
+	testServer := httptest.NewServer(r)
+	defer testServer.Close()
+
+	// GET
+	resp, err := http.Get(testServer.URL + "/ping")
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusNotFound, resp.StatusCode)
+	respBody, err := io.ReadAll(resp.Body)
+	assert.NoError(t, err)
+	defer resp.Body.Close()
+	assert.Equal(t, "", string(respBody))
+
+	// POST
+	resp, err = http.Post(testServer.URL+"/test", "application/json", bytes.NewBuffer([]byte("test")))
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	respBody, err = io.ReadAll(resp.Body)
+	assert.NoError(t, err)
+	defer resp.Body.Close()
+	assert.Equal(t, "success: test", string(respBody))
+
+	resp, err = http.Post(testServer.URL+"/not_found", "application/json", nil)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusNotFound, resp.StatusCode)
+	respBody, err = io.ReadAll(resp.Body)
+	assert.NoError(t, err)
+	defer resp.Body.Close()
+	assert.Equal(t, "", string(respBody))
+}
 
 func Test_router_SetMiddleware(t *testing.T) {
 	testFn := middleware(func(f http.HandlerFunc) http.HandlerFunc { return f })
