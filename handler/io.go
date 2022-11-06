c06174bcc1c6b190fac5b0e4c69c53fcf019946f
s-beats <sakashin10291029@gmail.com>
2022-11-06 13:20:10 +0900

    test write

--- handler/io.go
+++ handler/io.go
@@ -4,10 +4,16 @@ import "net/http"
 
 type HandlerInput struct{}
 
-type HandlerOutput struct{}
+type HandlerOutput struct {
+	StatusCode int
+	Body       []byte
+}
 
 func Wrap(handler func(input *HandlerInput) *HandlerOutput) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// TODO
+		output := handler(&HandlerInput{})
+		w.WriteHeader(output.StatusCode)
+		w.Write(output.Body)
 	}
 }
--- handler/user.go
+++ handler/user.go
@@ -1,11 +1,19 @@
 package handler
 
+import "net/http"
+
 func CreateUser(input *HandlerInput) *HandlerOutput {
 	// TODO
-	return &HandlerOutput{}
+	return &HandlerOutput{
+		StatusCode: http.StatusCreated,
+		Body:       []byte("{\"id\": 1, \"name\": \"John\"}"),
+	}
 }
 
 func GetUserList(input *HandlerInput) *HandlerOutput {
 	// TODO
-	return &HandlerOutput{}
+	return &HandlerOutput{
+		StatusCode: http.StatusOK,
+		Body:       []byte("[{\"id\": 1, \"name\": \"John\"}]"),
+	}
 }
