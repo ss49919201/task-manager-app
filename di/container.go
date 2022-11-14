a22aaf783e08fd6b85b4719d59071e59e28c6057
s-beats <sakashin10291029@gmail.com>
2022-11-14 22:29:38 +0900

    remove layer
    - repository
    - service
    - usecase

--- di/container.go
+++ di/container.go
@@ -3,10 +3,6 @@ package di
 import (
 	"sync"
 
-	"github.com/s-beats/rest-todo/domain/repository"
-	"github.com/s-beats/rest-todo/domain/service"
-	"github.com/s-beats/rest-todo/infra/rdb/persistence"
-	"github.com/s-beats/rest-todo/usecase"
 	"github.com/samber/do"
 	"xorm.io/xorm"
 )
@@ -28,25 +24,6 @@ func NewContainer(db *xorm.Engine) *Container {
 
 		// db
 		do.ProvideValue(container.Injector, db)
-
-		// repository
-		do.ProvideValue(container.Injector, persistence.NewUser(db))
-		do.ProvideValue(container.Injector, persistence.NewTask(db))
-
-		// service
-		do.ProvideValue(container.Injector, service.NewTask(
-			do.MustInvoke[repository.User](container.Injector),
-			do.MustInvoke[repository.Task](container.Injector),
-		))
-
-		// usecase
-		do.ProvideValue(container.Injector, usecase.NewTask(
-			do.MustInvoke[service.Task](container.Injector),
-			do.MustInvoke[repository.Task](container.Injector),
-		))
-		do.ProvideValue(container.Injector, usecase.NewUser(
-			do.MustInvoke[repository.User](container.Injector),
-		))
 	})
 
 	return container
